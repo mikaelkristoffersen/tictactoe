8457a8b10b9d170046bf4b93d49ab2c45f012b31
Mikael Kristoffersen <mikaelkristoffersen@gmail.com>
2020-09-13 22:35:56 +0200

    implemented and included ipynb

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,12 @@
 """
 Tic Tac Toe Player
 """
-
 import math
+import copy
 
 X = "X"
 O = "O"
 EMPTY = None
-
 
 def initial_state():
     """
@@ -17,51 +16,115 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
+positions = [(i,j) for i in range(0,3) for j in range(0,3)]
+
+def flatten(board):
+     return [field for row in board for field in row ]
+
+fieldState={
+    X : 1,
+    O : -1,
+    None : 0
+}
+
+def boardState(board):
+    return [fieldState[field] for field in flatten(board)]
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    # Count EMPTY fieds on the board
+    if sum(boardState(board)) == 0:
+        return X
+    return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    l = []
+    i = 0
+    for b in boardState(board):
+        if b == 0:
+           l.append(positions[i])
+        i = i +1
+    return l
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    newBoard = copy.deepcopy(board)
 
+    if action in actions(newBoard): 
+        newBoard[action[0]][action[1]] = player(newBoard)
+        return newBoard
+    raise Exception("Action not valid.")
+
+def square_combis(lst, n):
+    for i in range(0, len(lst), n):
+        yield sum(lst[i:i + n])
+    for i in range(0, n):
+        yield sum(lst[i::n])
+    yield sum(lst[0::n+1])
+    yield sum(lst[n-1:1-n:n-1])
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for s in square_combis(boardState(board),3):
+        if(s == 3):
+            return X
+        if(s == -3):
+            return O
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    return winner(board) !=  None or not actions(board)
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    return fieldState[winner(board)]
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    a = None
+    if terminal(board):
+        return a
+
+    isX = player(board) == X 
+    v = 2
+    sign = 1 if isX else -1
+        
+    for action in actions(board):
+        newValue = minmaxvalue(result(board, action), isX)
+        if v*sign <= newValue*sign: 
+            a = action
+            v = newValue
+    return a
+
+          
+def minmaxvalue(board, useMin):
+    if terminal(board):
+        return utility(board)
+    v = 2 if useMin else -2
+    for action in actions(board):
+        # if min-value call: min(v,max-value)
+        r = minmaxvalue(result(board, action), not useMin)
+        v = min(v, r) if useMin else max(v,r)
+    return v
+    
