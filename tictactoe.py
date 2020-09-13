93f8784ad61729fc03c0e14e1bc217be0cba84d2
Mikael Kristoffersen <mikaelkristoffersen@gmail.com>
2020-09-13 23:43:37 +0200

    opmitized

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,12 @@
 """
 Tic Tac Toe Player
 """
-import math
 import copy
 
 X = "X"
 O = "O"
 EMPTY = None
+
 
 def initial_state():
     """
@@ -16,19 +16,24 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
-positions = [(i,j) for i in range(0,3) for j in range(0,3)]
+
+positions = [(i, j) for i in range(0, 3) for j in range(0, 3)]
+
 
 def flatten(board):
-     return [field for row in board for field in row ]
+    return [field for row in board for field in row]
 
-fieldState={
-    X : 1,
-    O : -1,
-    None : 0
+
+fieldState = {
+    X: 1,
+    O: -1,
+    None: 0
 }
+
 
 def boardState(board):
     return [fieldState[field] for field in flatten(board)]
+
 
 def player(board):
     """
@@ -46,10 +51,15 @@ def actions(board):
     """
     l = []
     i = 0
-    for b in boardState(board):
+    bs = boardState(board)
+    # we can optimize initial action to: [(1,1), (0,1), (0,0)]
+    # since this represtents all possible startes with a transpose of the board
+    if(all(b == 0 for b in bs)):
+        return [(1,1),(0,1),(0,0)]
+    for b in bs:
         if b == 0:
-           l.append(positions[i])
-        i = i +1
+            l.append(positions[i])
+        i = i + 1
     return l
 
 
@@ -59,12 +69,13 @@ def result(board, action):
     """
     newBoard = copy.deepcopy(board)
 
-    if action in actions(newBoard): 
+    if action in actions(newBoard):
         newBoard[action[0]][action[1]] = player(newBoard)
         return newBoard
     print(action)
     print(board)
     raise Exception("Action not valid.")
+
 
 def square_combis(lst, n):
     for i in range(0, len(lst), n):
@@ -74,11 +85,12 @@ def square_combis(lst, n):
     yield sum(lst[0::n+1])
     yield sum(lst[n-1:1-n:n-1])
 
+
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    for s in square_combis(boardState(board),3):
+    for s in square_combis(boardState(board), 3):
         if(s == 3):
             return X
         if(s == -3):
@@ -90,7 +102,7 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    return winner(board) !=  None or not actions(board)
+    return winner(board) != None or not actions(board)
 
 
 def utility(board):
@@ -108,34 +120,31 @@ def minimax(board):
     if terminal(board):
         return a
 
-    maximize = player(board) == X 
+    maximize = player(board) == X
     sign = -1 if maximize else 1
-    old_value = 2*sign
-        
+    prev_value = 2*sign
+
     for action in actions(board):
         # The maximizing player picks action a in Actions(s) that
         # produces the highest value of Min-Value(Result(s, a)).
-        # The minimizing player picks action a in Actions(s) that 
+        # The minimizing player picks action a in Actions(s) that
         # produces the lowest value of Max-Value(Result(s, a)).
-        newValue = minmaxvalue(result(board, action), maximize)
-        if(maximize):
-            if old_value <= newValue: 
-                a = action
-                old_value = newValue
-        else:
-            if  old_value >= newValue: 
-                a = action
-                old_value= newValue
+        newValue = minmaxvalue(result(board, action), maximize, prev_value)
+        if prev_value*sign >= newValue*sign:
+            a = action
+            prev_value = newValue
     return a
 
-          
-def minmaxvalue(board, minValue):
+
+def minmaxvalue(board, minValue, prev_val=0):
     if terminal(board):
         return utility(board)
     v = 2 if minValue else -2
     for action in actions(board):
         # if min-value call: min(v,max-value)
-        r = minmaxvalue(result(board, action), not minValue)
+        r = minmaxvalue(result(board, action), not minValue, v)
         v = min(v, r) if minValue else max(v, r)
+        # Alpha-Beta Pruning skips some of the recursive computations that are decidedly unfavorable. 
+        if(minValue and v < prev_val) or (not minValue and v > prev_val):
+            break
     return v
-    
